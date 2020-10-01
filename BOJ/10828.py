8310cadf55926e5fce756425803a57fa9a0657ae
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-10-01 17:41:01 +0900

    10828. 스택

---
+++ BOJ/10828.py
@@ -0,0 +1,47 @@
+'''
+Stack구현
+1. push X: 정수 X를 스택에 넣는 연산이다.
+2. pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+3. size: 스택에 들어있는 정수의 개수를 출력한다.
+4. empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+5. top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+'''
+
+import sys
+
+def push(x):
+    stack.append(x)
+
+def pop():
+    if(not stack):
+        return -1
+    else:
+        return stack.pop()
+
+def size():
+    return len(stack)
+
+def empty():
+    return 0 if stack else 1
+
+def top():
+    return stack[-1] if stack else -1
+
+N = int(sys.stdin.readline().rstrip())
+stack = []
+
+for _ in range(N):
+    input_split = sys.stdin.readline().rstrip().split()
+
+    order = input_split[0]
+
+    if order == "push":
+        push(input_split[1])
+    elif order == "pop":
+        print(pop())
+    elif order == "size":
+        print(size())
+    elif order == "empty":
+        print(empty())
+    elif order == "top":
+        print(top())
