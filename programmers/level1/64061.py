691bdfd7258e6da459a501780e50a625d7915064
Hyewon <60416187+hyewonji@users.noreply.github.com>
2021-01-13 00:01:08 +0900

    64061 크레인 인형뽑기 게임

--- programmers/level1/64061.py
+++ programmers/level1/64061.py
@@ -2,14 +2,14 @@
 크레인 인형뽑기 게임
 '''
 
+
 def solution(board, moves):
 
     arry = []
     for i in moves:
       for j in range(len(board)):
-        s = board[j][i-1]
-        if s != 0:
-          arry.append(s)
+        if board[j][i-1] != 0:
+          arry.append(board[j][i-1])
           board[j][i-1] = 0
           break
 
@@ -17,11 +17,34 @@ def solution(board, moves):
     n = 1
     while n < len(arry):
         if arry[n] == arry[n-1]:
-          r += 1
+          r += 2
           del arry[n]
           del arry[n-1]
           if n != 1:
             n -= 1
         else:  
           n += 1  
-    return r*2
+    return r
+
+
+'''
+stack을 사용해서 푸는 방법
+'''
+def solution(board, moves):
+    stacklist = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2     
+                break
+
+    return answer
